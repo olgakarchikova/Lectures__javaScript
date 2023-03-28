183366a471f91bdd2bcde2e72278fc60994bbce8
Karchikova_Olga <olgasolo1@yandex.ru>
2023-03-28 18:19:12 +0300

    initial commit

---
+++ js.js
@@ -0,0 +1,54 @@
+// console.log("Текст из js.js")
+
+// let b = 1 + 2 + 3;
+// console.log(b);
+
+// const num = 123 + 10;
+// console.log(num);
+
+// let a = 6,
+//     b = 7,
+//     c = 9;
+// console.log(a,b,c);
+
+// let [a, b, c] = [1, 2, 3]
+// console.log(a,b,c);
+
+// const c = 10,
+//     d = 2;
+// const sum = (c + d);
+// // console.log(sum);
+// const sub = (c - d);
+// // console.log(sub);
+// const mult = (c * d);
+// // console.log(mult);
+// const div = (c / d);
+// // console.log(div)
+// const res = sum + sub + mult + div;
+// // console.log('Результат сложения всех переменных: ' + sum, sub, mult, div);
+// // console.log('Результат сложения всех переменных: ' + res);
+// console.log(`Сумма = ${sum} 
+// Разность = ${sub} 
+// Произведение = ${mult} 
+// Частное = ${div}`)
+
+// Создать переменную а, записать в нее какое-нибудь число, вывести на экран это число с обратным знаком.
+// const a = 5;
+// const num = a * (-1);
+// console.log(num);
+
+// Создать 2 переменные, записать Имя и Фамилию, вывести через консоль.
+const userName = 'Ольга';
+const surname = 'Карчикова';
+// console.log(`${userName} ${surname}`);
+console.log(userName+ ', '+surname + '!');
+
+// Создать 2 переменные, записать текст, вывести через консоль.
+const txt = 'Java',
+    tekst =  'script';
+console.log(`${txt} ${tekst}`);
+
+const a = 'hello',
+    b = 'world';
+// console.log(`${a} ${b}`);
+console.log(a+', '+b + '!');
